d48bd3e24652247e53b44fd39ef4cb98ef501ff1
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-17 10:07:32 +0200

    fix: fix function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
     let str = "" + n;
-    let str1 = str.reverse();
+    let str1 = str.split("").reverse().join("");
     return Number(str1);
 };
